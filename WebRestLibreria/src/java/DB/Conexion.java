43dcdf64cac742a9e2927425a61bb0fcefec40c9
Nikodeliko <44078433+Nikodeliko@users.noreply.github.com>
2019-04-23 21:51:31 -0400

    Conexion Testeada y funcionando
    Saludos!

--- WebRestLibreria/src/java/DB/Conexion.java
+++ WebRestLibreria/src/java/DB/Conexion.java
@@ -13,19 +13,22 @@ public class Conexion {
     private static Connection con = null;
     
     public static Connection getConexion(){
-      try{
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String connectionUrl = "jdbc:sqlserver://DESKTOP-0A3F1TV\\SQLEXPRESS:1433"+
-            "databaseName=CASOLIBROS;user=sa;password=q1w2e3r4.3102;";
-        con = DriverManager.getConnection(connectionUrl);
-      } catch (SQLException e){
-          System.out.println("SQL Exception: "+e.toString());
-      } catch (ClassNotFoundException cE){
-          System.out.println("Class Not Found Exception: "+ cE.toString());
-      }
-      return con;
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=CASOLIBROS";
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }catch (ClassNotFoundException e){
+            System.out.println("Class Not Found Exception: "+ e.toString());
+        }
+        
+        try{
+            con = DriverManager.getConnection(url,"sa","q1w2e3r4.3102");
+        }catch(SQLException ex){
+            System.out.println("SQL Exception: "+ex.toString());
+        }
+        
+        return con;
     }
-    
+
     public static void CloseConexion(){
         try{
             con.close();
